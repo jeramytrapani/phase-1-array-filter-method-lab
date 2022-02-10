52a4c19ca12dc10f4ca59150464d212bc1efb10b
jeramytrapani <jeramytrapani@gmail.com>
2022-02-10 09:34:56 -0700

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching (drivers, target) {
+    const result = drivers.filter((name)=>{return name.toLowerCase() === target.toLowerCase()})
+    return result
+
+}
+findMatching(drivers, "Bobby");
+
+function fuzzyMatch (drivers, searchString) {
+    const result = drivers.filter((name)=> name.startsWith(searchString));
+    return result;
+}
+
+fuzzyMatch(drivers, "Sa");
+//console.log(result);
+
+function matchName (drivers, name) {
+    const result = drivers.filter((driver)=> driver.name === name);
+    return result
+}
+
+matchName(drivers,'Bobby')
